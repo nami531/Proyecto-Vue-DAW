d4cee47327ec614dc1300f7f69c3dc1563804ab0
Nadia Marquez <nadiamarquez27112005@gmail.com>
2025-02-11 19:51:29 +0100

    feat: Modificar cantidad

--- src/components/TablaCarrito.vue
+++ src/components/TablaCarrito.vue
@@ -1,25 +1,22 @@
 <template>
-    <div class="container">
-        <table class="table table-striped mt-2">
+    <div class="container" v-if="cartStore.items.length > 0">
+        <table class="table table-striped mt-2" >
                 <thead>
                     <tr class="table-primary">
-                        <th scope="col" class="w-10 text-center align-middle">Foto</th>
                         <th scope="col" class="w-20 text-start  align-middle">Nombre</th>
                         <th scope="col" class="w-45 text-center align-middle">Descripción</th>
                         <th scope="col" class="w-10 text-center align-middle">Precio</th>
-                        <th scope="col" class="w-10 text-center align-middle">Stock</th>
-                        <th scope="col" class="table-info text-center align-middle">Comprar</th>
+                        <th scope="col" class="w-10 text-center align-middle">Foto</th>
+                        <th scope="col" class="w-10 text-center align-middle">Cantidad</th>
+                        <th scope="col" class="table-info text-center align-middle">Eliminar</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="item in this.cartStore.getItems" :key="item.id">
-                        <td class="d-flex justify-content-center align-items-center align-middle">
-                            <img :src="`http://localhost:5000/uploads/img/${item.imagen}`" :alt="item.nombre" width="100px" height="100px">
-                        </td>
                         <td class="text-center align-middle">{{ item.nombre }}</td>
                         <td class="text-center align-middle">{{ item.descripcion }}</td>
                         <td class="text-center align-middle">{{ item.precio_unitario }} &euro;</td>
-                        <td class="text-center align-middle">{{ item.stock_disponible }}</td>
+                        
                         <td>
                             <img :src="`http://localhost:5000/uploads/img/${item.imagen}`" alt="Foto de producto" width="64" height="64" class="img-thumbnail" 
                             @click="openModal()"/>
@@ -28,14 +25,28 @@
                         <div v-if="isModalOpen" class="modal" @click="closeModal">
                             <img :src="`http://localhost:5000/uploads/img/${item.imagen}`" alt="Foto expandida" class="modal-content" />
                         </div>
+                        <td class="align-middle">
+                            <div class="input-group gap-2 d-flex align-items-center justify-content-center ">
+                                <button @click="decrease(item)" class="btn btn-primary p-0 px-2">-</button>
+                                <span class="fs-5">{{ item.quantity }}</span>
+                                <button @click="increase(item)" class="btn btn-primary p-0 px-2  ">+</button>
+                            </div>
+                        </td>
                         <td class="text-center align-middle table-info">
-                            <button class="btn btn-success" @click="agregarArticulo(item)">
-                                <i class="fas fa-shopping-cart"></i>
+                            <button class="btn btn-danger" @click="eliminarArticulo(item)">
+                                <i class="bi bi-trash"></i>
                             </button>
                         </td>
                     </tr>
                 </tbody>
             </table>
+            <h2 class="text-end"> Precio total: {{ cartStore.totalPrice.toFixed(2) }} €</h2>
+    </div>
+    <div v-else>
+        <div class="d-flex flex-column align-items-center justify-content-center min-vh-100">
+            <i class="bi bi-dropbox fs-1"></i>
+            <h3>El carrito está vacío</h3>
+        </div>
     </div>
 </template>
 
@@ -56,6 +67,22 @@ export default ({
 
     mounted(){
         console.log("Items del carrito", this.cartStore.getItems)
+    }, 
+
+    methods : {
+        eliminarArticulo(articulo){
+            this.cartStore.removeFromCart(articulo.id)
+        },
+
+        increase(articulo){
+            this.cartStore.updateQuantity(articulo.id, articulo.quantity + 1)
+        }, 
+
+        decrease(articulo){
+            this.cartStore.updateQuantity(articulo.id, articulo.quantity - 1)
+        },
+
+        
     }
 })
 </script>
