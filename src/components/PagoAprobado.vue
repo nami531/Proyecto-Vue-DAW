e1f09f28d1d9fde874703a8f099412f2ce41a42b
nami531 <nadiamarquez27112005@gmail.com>
2025-02-27 09:09:35 +0100

    style: Pago aprobado centrado

--- src/components/PagoAprobado.vue
+++ src/components/PagoAprobado.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="text-center p-4 bg-white shadow rounded">
-        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="green" class="bi bi-check-circle-fill"
-            viewBox="0 0 16 16">
-            <path
-                d="M16 8a8 8 0 1 1-16 0 8 8 0 0 1 16 0zM6.354 11.354a.5.5 0 0 0 .707 0l4.5-4.5a.5.5 0 1 0-.707-.707L6.5 10.293 4.146 7.94a.5.5 0 1 0-.707.707l2.915 2.707z" />
-        </svg>
-        <h1 class="mt-3">¡Pago realizado con éxito!</h1>
-        <p class="text-muted">Gracias por tu compra. Hemos recibido tu pago correctamente.</p>
-        <p>Descargue su factura en formato PDF: </p>
-        <button @click="generarFacturaPdf" class="btn btn-primary">
-            <i class="bi bi-file-earmark-pdf"></i>Descargar factura
-        </button>
-        <router-link to="/" class="btn btn-success mt-3">Volver al inicio</router-link>
+    <div class="container">
+        <div class="text-center p-4 bg-white shadow rounded mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="green" class="bi bi-check-circle-fill"
+                viewBox="0 0 16 16">
+                <path
+                    d="M16 8a8 8 0 1 1-16 0 8 8 0 0 1 16 0zM6.354 11.354a.5.5 0 0 0 .707 0l4.5-4.5a.5.5 0 1 0-.707-.707L6.5 10.293 4.146 7.94a.5.5 0 1 0-.707.707l2.915 2.707z" />
+            </svg>
+            <h1 class="mt-3">¡Pago realizado con éxito!</h1>
+            <p class="text-muted">Gracias por tu compra. Hemos recibido tu pago correctamente.</p>
+            <p>Descargue su factura en formato PDF: </p>
+            <button @click="generarFacturaPdf" class="btn btn-primary">
+                <i class="bi bi-file-earmark-pdf"></i>Descargar factura
+            </button><br>
+            <router-link to="/" class="btn btn-success mt-3">Volver al inicio <i class="bi bi-arrow-return-left"></i></router-link>
+        </div>
     </div>
+    
 </template>
 
 <script>
